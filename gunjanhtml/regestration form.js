798631721ff52e8e9cf0ef4f3fb4723583d56665
Gunjan Patel <123575660+GunjanPatel359@users.noreply.github.com>
2023-12-16 11:45:19 +0530

    Add files via upload

---
+++ gunjanhtml/regestration form.js
@@ -0,0 +1,7 @@
+let details={};
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let gen=document.querySelector('input[name="gen"]:checked').value;
+function submit(){
+    console.log(gen);
+}
